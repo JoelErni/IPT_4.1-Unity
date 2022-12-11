134420a09c8fdd6ae477f598e42696b12c081500
Joel Erni <102746806+JoelErni@users.noreply.github.com>
2022-12-11 17:04:16 +0100

    Update Timer.cs

--- IPT-4.1Basis5/Assets/script/Timer.cs
+++ IPT-4.1Basis5/Assets/script/Timer.cs
@@ -3,26 +3,34 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
+  // Zeit, die noch übrig ist
   public float timeRemaining = 10;
+  // Timer läuft gerade
   public bool timerIsRunning = false;
+  // Textkomponente, die die Zeit anzeigt
   public Text timeText;
+  // Referenz auf UI-Skript
   public UI ui;
 
+  // Startet den Timer beim Start des Spiels
   private void Start()
   {
     // Starts the timer automatically
     timerIsRunning = true;
     if (SceneManager.GetActiveScene().name != "IntroScene")
     {
+      // Lädt gespeicherte Zeit aus dem PlayerPrefs
       timeRemaining = PlayerPrefs.GetFloat("Timer");
     }
     }
+  // Aktualisiert die Zeit jeden Frame
   void Update()
   {
     if (timerIsRunning)
     {
       if (timeRemaining > 0)
       {
+        // Zeit wird pro Frame um deltaTime reduziert
         timeRemaining -= Time.deltaTime;
         DisplayTime(timeRemaining);
       }
@@ -34,8 +42,11 @@ public class Timer : MonoBehaviour
       }
     }
   }
+  
+  // Zeigt die Zeit in der Textkomponente an
   void DisplayTime(float timeToDisplay)
   {
+    // Korrigiert die angezeigte Zeit um 1 Sekunde
     timeToDisplay += 1;
     float minutes = Mathf.FloorToInt(timeToDisplay / 60);
     float seconds = Mathf.FloorToInt(timeToDisplay % 60);
